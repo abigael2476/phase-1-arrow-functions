1a5b89e64686cb830aa947e4d429b28a823192ba
abigaelseenoi <abigaelseenoi5@gmail.com>
2025-09-08 18:38:55 +0300

    addedarrows

---
+++ index.js
@@ -0,0 +1,10 @@
+// Function expression
+const divide = function () {
+  return 2000 / 100;
+};
+
+// Arrow function with one parameter
+const square = (x) => x * x;
+
+// Arrow function with two parameters
+const add = (a, b) => a + b;
